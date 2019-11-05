5f1fd276170fc941145f88fbacf8ce06b15ebfd4
Eileen Trampe <ebtrampe@gmail.com>
2019-11-05 12:15:25 +0800

    Updates

---
+++ precache-manifest.ad5d34c4f29d492fa11576d394414482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdfc44cc969a5bd9936c30669a1f3f6f",
+    "url": "/react-contacts-manager/index.html"
+  },
+  {
+    "revision": "6cf94d5c70a4972959b8",
+    "url": "/react-contacts-manager/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "16b750cc6edd683e8460",
+    "url": "/react-contacts-manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "6cf94d5c70a4972959b8",
+    "url": "/react-contacts-manager/static/js/2.4e6c62bb.chunk.js"
+  },
+  {
+    "revision": "16b750cc6edd683e8460",
+    "url": "/react-contacts-manager/static/js/main.6b4200b1.chunk.js"
+  },
+  {
+    "revision": "5976ba1d4209bbaf3d77",
+    "url": "/react-contacts-manager/static/js/runtime-main.97aa7c7f.js"
+  }
+]);
